c39fb59e1a18b3d36bea783b89f36dd3bb584d0a
Juan David <juangalindo28@hotmail.com>
2022-06-27 11:47:53 -0500

    Creando la clase madre

--- ecommerceBackend/src/main/java/com/juan/ecommerceBackend/models/BaseEntity.java
+++ ecommerceBackend/src/main/java/com/juan/ecommerceBackend/models/BaseEntity.java
@@ -1,5 +1,8 @@
 package com.juan.ecommerceBackend.models;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 import java.util.Date;
 
@@ -8,6 +11,7 @@ public class BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="id", updatable = false, nullable = false)
+    @Getter @Setter
     private long id;
     @Column(columnDefinition = "DATETIME", updatable= false, nullable = false)
     protected Date createdDate;
