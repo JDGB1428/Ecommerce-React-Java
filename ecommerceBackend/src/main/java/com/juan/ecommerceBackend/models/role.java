696e66aedfd002a1b100206c571413c5efc7f54e
Juan David <juangalindo28@hotmail.com>
2022-06-27 11:50:40 -0500

    Agregando lombok y ORM

--- ecommerceBackend/src/main/java/com/juan/ecommerceBackend/models/role.java
+++ ecommerceBackend/src/main/java/com/juan/ecommerceBackend/models/role.java
@@ -1,4 +1,24 @@
 package com.juan.ecommerceBackend.models;
 
-public class role {
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.JsonIdentityReference;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name="role")
+public class role extends BaseEntity{
+    @Column(name="nickname")
+    @Getter @Setter
+    private String nickname;
+
+    @OneToMany(cascade = CascadeType.ALL
+            , fetch = FetchType.EAGER, mappedBy = "role")
+
+    @Getter @Setter
+    private Set<permiso> permisos;
 }
