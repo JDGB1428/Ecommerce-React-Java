cf241c1e00ec8ea73e68a4eded8832f078828f05
Juan David <juangalindo28@hotmail.com>
2022-06-27 11:49:20 -0500

    añadiendo lombok

--- ecommerceBackend/src/main/java/com/juan/ecommerceBackend/models/user.java
+++ ecommerceBackend/src/main/java/com/juan/ecommerceBackend/models/user.java
@@ -1,23 +1,32 @@
 package com.juan.ecommerceBackend.models;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
 import javax.persistence.*;
-import java.util.Date;
 
-@Getter
-@Setter
 @Entity
 @Table(name = "user")
+@ToString
+@EqualsAndHashCode
 @AllArgsConstructor
 @NoArgsConstructor
-public class User extends BaseEntity{
+public class user extends BaseEntity{
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name="role_id")
+    private role role;
+
     @Column(name="nickname")
     private String nickname;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     @JsonIgnore
     @Column(name="password")
+    @Getter @Setter
     private String password;
     @Column(name="correo")
+    @Getter @Setter
     private String correo;
 }
