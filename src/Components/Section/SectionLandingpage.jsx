02928586536a741655cca74e1b0f9a3ce160fa01
Juan David <juangalindo28@hotmail.com>
2022-06-28 18:16:16 -0500

    Añadiendo la secciones de la pagina

---
+++ src/Components/Section/SectionLandingpage.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import '../Section/SectionStyles.css';
+
+const SectionLandingpage = () => {
+  return (
+    <>
+      <section id ="Inicio">
+        <div className="section-introduce">
+          <h2>Hazte la vida</h2>
+          <p>mas facil con nosotros</p>
+        </div>
+      </section>
+      <hr/>
+      <section id ="Acerca de">
+        <div className="section-introduce">
+          <h2>Hazte la vida</h2>
+          <p>mas facil con nosotros</p>
+        </div>
+      </section>
+      <hr/>
+      <section id ="Testimonio">
+        <div className="section-introduce">
+          <h2>Hazte la vida</h2>
+          <p>mas facil con nosotros</p>
+        </div>
+      </section>
+      <hr/>
+    </>
+  )
+}
+
+export default SectionLandingpage
