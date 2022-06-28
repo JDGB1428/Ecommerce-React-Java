7b5d4777832c4197fc2ca9e55dd5cba0b8866a84
Juan David <juangalindo28@hotmail.com>
2022-06-28 18:15:16 -0500

    escribiendo los item para la nav

---
+++ src/Components/Navbar/data.js
@@ -0,0 +1,19 @@
+export const links=[
+{
+    id:1,
+    text:'Inicio',
+    url:'#Inicio'
+},{
+    id:2,
+    text:'Acerca de',
+    url:'#Acerca de'
+},{
+    id:3,
+    text:'Testimonio',
+    url:'#Testimonio'
+},{
+    id:4,
+    text:'Contacto',
+    url:'#Contacto'
+}
+]
